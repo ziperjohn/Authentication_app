65712dd87991ca394e49e3758925ffbbca21885c
ziperjohn <honzavaverka1996@gmail.com>
2021-07-07 22:48:11 +0200

    ADDED ToDo to auth service

--- lib/service/authentication_service.dart
+++ lib/service/authentication_service.dart
@@ -3,6 +3,8 @@ import 'package:firebase_auth/firebase_auth.dart';
 class AuthenticationService {
   final FirebaseAuth _auth = FirebaseAuth.instance;
 
+  //Sign in anonymously, Firebase create a new user that will be persisted across app restart.
+  //If the user signs in anonymously multiple times, they will be signed-in with the initially created account.
   Future<dynamic> signInAnonymously() async {
     try {
       UserCredential userCredential = await _auth.signInAnonymously();
@@ -12,4 +14,14 @@ class AuthenticationService {
       return null;
     }
   }
+
+  //TODO Sign in by email & password
+
+  //TODO Registration by email & password
+
+  //TODO Sign in by google
+
+  //TODO Sign in by Facebook
+
+  //TODO Sign in by Apple
 }
